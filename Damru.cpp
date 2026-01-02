bba7cc33c2da3ea087f8978e668b41ec0d979aa3
Ayush <ayushpandit672@gmail.com>
2026-01-02 23:06:10 +0530

    Damru

---
+++ Damru.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    for(int i=1;i<=x-1;i++){
+        for(int s=1;s<i;s++){
+            cout<<"  ";
+        }
+        for(int j=1;j<=(x*2)-2*i+1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<=x;i++){
+        for(int s=1;s<=x-i;s++){
+            cout<<"  ";
+        }
+        for(int j=1;j<=i*2-1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
